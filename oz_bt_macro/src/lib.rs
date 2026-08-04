590f70fda73b9514f949dd246e9ef99bb39a19ae
Ömer Faruk Öz <ozomerfaruk98@gmail.com>
2026-08-04 13:35:21 +0300

    remove old macro

--- oz_bt_macro/src/lib.rs
+++ oz_bt_macro/src/lib.rs
@@ -1,12 +1,11 @@
 use heck::ToSnakeCase;
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
 use syn::{
     parse::{Parse, ParseStream},
     parse_macro_input,
     punctuated::Punctuated,
-    Expr, ExprLit, ImplItem, ImplItemType, ItemImpl, ItemMod, Lit, Meta, Token,
+    Expr, ExprLit, ImplItem, ImplItemType, ItemImpl, Lit, Meta, Token,
 };
 
 fn has_assoc_type(input_impl: &syn::ItemImpl, name: &str) -> bool {
@@ -176,139 +175,4 @@ pub fn node(attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     expanded.into()
-}
-
-#[proc_macro_attribute]
-pub fn node_(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemMod);
-    let mod_name = &input.ident;
-    let mod_name_string = mod_name.to_string();
-
-    let content = if let Some((_, ref items)) = input.content {
-        let content_tokens = items
-            .iter()
-            .map(|item| quote! { #item })
-            .collect::<TokenStream2>();
-        content_tokens
-    } else {
-        TokenStream2::new()
-    };
-
-    let expanded: TokenStream2 = quote! {
-        pub mod #mod_name {
-
-            #content
-
-            pub use lib::NodeManager;
-
-            trait CustomNode {
-                fn start(&mut self, _: &mut CustomData) {}
-                fn execute(&mut self, _: &mut CustomData) -> crate::exec::States;
-                fn end(&mut self, _: &mut CustomData) {}
-            }
-
-            struct CustomData<'a> {
-                data: &'a mut Data,
-                input: &'a Input,
-                output: &'a mut Output,
-            }
-
-            pub fn new(
-                input: impl Fn(&mut Data) -> Input + 'static,
-            ) -> Box<NodeManager> {
-                NodeManager::new(input)
-            }
-
-            pub mod lib {
-                impl NodeManager {
-                    pub fn new(
-                        input: impl Fn(&mut super::Data) -> super::Input + 'static,
-                    ) -> Box<Self> {
-                        Box::new(NodeManager {
-                            output_handle: std::rc::Rc::new(std::cell::RefCell::new(super::Output::default())),
-                            input_handle: Box::new(input),
-                            node: None,
-                            comment: None,
-                        })
-                    }
-
-                    pub fn comment(mut self: Box<Self>, comment: &str) -> Box<Self> {
-                        self.comment = Some(comment.to_string());
-                        self
-                    }
-
-                    pub fn with_output(mut self: Box<Self>, output: std::rc::Rc<std::cell::RefCell<super::Output>>) -> Box<Self>{
-                        self.output_handle = output;
-                        self
-                    }
-                }
-
-                pub struct NodeManager {
-                    pub input_handle: Box<dyn Fn(&mut super::Data) -> super::Input>,
-                    pub output_handle: std::rc::Rc<std::cell::RefCell<super::Output>>,
-                    pub node: Option<Box<dyn super::CustomNode>>,
-                    pub comment: Option<String>,
-                }
-
-                impl ::oz_bt::executable::exec::Executable<super::Data> for NodeManager {
-                    fn start(&mut self, data: &mut super::Data) {
-                        self.node = Some(Box::new(super::Node::default()));
-                        let input_data = self.input_handle.as_ref()(data);
-                        let mut output_data = self.output_handle.borrow_mut();
-                        let mut custom_data = super::CustomData {
-                            input: &input_data,
-                            output: &mut output_data,
-                            data: data,
-                        };
-                        self.node.as_mut().unwrap().start(&mut custom_data);
-                    }
-
-                    fn execute(&mut self, data: &mut super::Data) -> ::oz_bt::executable::exec::States {
-                        let input_data = self.input_handle.as_ref()(data);
-                        let mut output_data = self.output_handle.borrow_mut();
-                        let mut custom_data = super::CustomData {
-                            input: &input_data,
-                            output: &mut output_data,
-                            data: data,
-                        };
-                        self.node.as_mut().unwrap().execute(&mut custom_data)
-                    }
-
-                    fn end(&mut self, data: &mut super::Data) {
-                        let input_data = self.input_handle.as_ref()(data);
-                        let mut output_data = self.output_handle.borrow_mut();
-                        let mut custom_data = super::CustomData {
-                            input: &input_data,
-                            output: &mut output_data,
-                            data: data,
-                        };
-                        self.node.as_mut().unwrap().end(&mut custom_data);
-                        self.node = None;
-                    }
-                }
-
-                impl ::oz_bt::executable::exec::ExecutableWatch for NodeManager {
-                    fn get_content(&self) -> ::oz_bt::executable::exec::WatchContent {
-                        ::oz_bt::executable::exec::WatchContent {
-                            node_type: ::oz_bt::executable::exec::NodeTypes::Leaf,
-                            name: #mod_name_string.to_string(),
-                            watch_state: ::oz_bt::executable::exec::WatchState::None,
-                            childs: Vec::new(),
-                            comment: self.comment.clone(),
-                        }
-                    }
-                }
-            }
-        }
-
-        #[macro_export]
-        macro_rules! #mod_name {
-            ( $x:expr $(,)? ) => {
-                #mod_name::new($x)
-            };
-        }
-
-    };
-
-    TokenStream::from(expanded)
 }
